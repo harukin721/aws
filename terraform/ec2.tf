9614ee63ec91791c3413a870239424247032a921
harukin721 <h.ueji.43@gmail.com>
2023-04-30 06:30:54 +0000

    terraform plan の差分確認用のEC2追加

---
+++ terraform/ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "ec2" {
+  # Amazon Linux 2023 AMI 2023.0.20230419.0 x86_64 HVM kernel-6.1
+  ami           = "ami-01b32aa8589df6208"
+  instance_type = "t2.micro"
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = 30
+  }
+}
