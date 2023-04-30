c86f90f162281abe8f541905c5aeced4446d2fa4
harukin721 <h.ueji.43@gmail.com>
2023-04-30 09:22:44 +0000

    terraform apply 追加
    - 承認して、EC2を1台立ててみる

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  count = 0
+  count = 1
   # Amazon Linux 2023 AMI 2023.0.20230419.0 x86_64 HVM kernel-6.1
   ami           = "ami-01b32aa8589df6208"
   instance_type = "t2.micro"
