138f7b3087091b73865f9bc6560bc2e8209f1401
harukin721 <h.ueji.43@gmail.com>
2023-05-14 13:53:58 +0000

    vpc 作成

---
+++ terraform/vpc.tf
@@ -0,0 +1,94 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-northeast-1a", "ap-northeast-1c"]
+  # 3subnets
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
+  database_subnets  = ["10.0.21.0/24", "10.0.22.0/24"]
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+# 外部からの HTTP,HTTPSアクセスを許可
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "allow http,https access."
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "allow http access."
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "allow https access."
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "alb"
+  }
+}
+
+# 上記に関連付けられた SG からの内部アクセスを許可
+resource "aws_security_group" "internal" {
+  name        = "allow_internal"
+  description = "allow internal access."
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description     = "allow internal access."
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.allow_http.id]
+  }
+  
+  ingress {
+    description     = "Allow internal HTTPS access."
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.allow_http.id]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "internal"
+  }
+}
